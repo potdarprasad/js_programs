9c9234dd17509df849872bfbbd80563d71918800
Prasad Potdar <prasadmpotdar22@gmail.com>
2022-11-01 10:28:19 +0530

    added pattern

---
+++ PatternPrograms/verticallyjoinedrightangledtriangles.js
@@ -0,0 +1,30 @@
+/**
+ * 1.
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * *
+    * * * * 
+    * * * 
+    * *
+    *
+ */
+let str = '';
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        str += '* ';
+    }
+    str += '\n';
+}
+
+
+for (let i = 4; i >= 1; i--) {
+    for (let j = 1; j <= i; j++) {
+        str += '* ';
+    }
+    str += '\n';
+}
+
+console.log(str);
